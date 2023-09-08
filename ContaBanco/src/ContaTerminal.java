9c9b84cecd46d4f4dd832463e4c1d64513a1d5e5
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2023-09-08 12:27:58 -0300

    initial commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -9,7 +9,7 @@ public class ContaTerminal {
         String nomeCliente= tec.next();
         System.out.println("Por favor, digite o número da Agência: ");
         int numeroAgencia= tec.nextInt();
-        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %2.3f já está disponível para saque", nomeCliente, agencia, numeroAgencia, saldo);
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %2.2f já está disponível para saque", nomeCliente, agencia, numeroAgencia, saldo);
 
     }
 }
